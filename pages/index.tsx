8be66acb3a63e32928d3b7a6e918278d8cfbb2b3
Clas Olaf Steinbru Andersen <kosec@LenovoC940Clas.localdomain>
2022-09-22 15:09:01 +0200

    feat: changed title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Home Page</div>;
+  return <div className="w-full h-full text-center flex flex-col justify-center">
+    <h1 className="text-2xl">Clas Olaf Steinbru Andersen</h1>
+    <h3>Software Engineer</h3>
+  </div>;
 };
 
 export default Home;
