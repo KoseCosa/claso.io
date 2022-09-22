34da90580b3820284d3d0bb8b443f45a5f8a5c64
Clas Olaf Steinbru Andersen <kosec@LenovoC940Clas.localdomain>
2022-09-22 15:05:32 +0200

    feat: added some links

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,30 +8,32 @@ import {
   FaEnvelopeSquare,
 } from "react-icons/fa";
 
+const iconSize = 42;
+
 function Footer() {
   return (
     <footer className="w-full bottom-0 p-2 bg-slate-50">
-      <nav className="flex items-center justify-center w-full sm:w-auto space-x-2">
-        <a>
-          <FaGithubSquare color="#24292f" size={28} />
+      <nav className="flex items-center justify-center w-full sm:w-auto space-x-4">
+        <a href="https://github.com/KoseCosa/">
+          <FaGithubSquare color="#24292f" size={iconSize} />
+        </a>
+        <a href="https://www.facebook.com/kosecosa">
+          <FaFacebookSquare color="#4267B2" size={iconSize} />
+        </a>
+        <a href="https://www.linkedin.com/in/kosecosa/">
+          <FaLinkedin color="0077B5" size={iconSize} />
+        </a>
+        <a href="https://twitter.com/kosecosa">
+          <FaTwitterSquare color="1DA1F2" size={iconSize} />
+        </a>
+        <a href="https://www.instagram.com/kosecosa/">
+          <FaInstagramSquare color="#F56040" size={iconSize} />
+        </a>
+        <a href="https://www.snapchat.com/add/kosecosa">
+          <FaSnapchatSquare color="#FFFC00" size={iconSize} />
         </a>
         <a>
-          <FaFacebookSquare color="#4267B2" size={28} />
-        </a>
-        <a>
-          <FaLinkedin color="0077B5" size={28} />
-        </a>
-        <a>
-          <FaTwitterSquare color="1DA1F2" size={28} />
-        </a>
-        <a>
-          <FaInstagramSquare color="#F56040" size={28} />
-        </a>
-        <a>
-          <FaSnapchatSquare color="#FFFC00" size={28} />
-        </a>
-        <a>
-          <FaEnvelopeSquare color="#24292f" size={28} />
+          <FaEnvelopeSquare color="#24292f" size={iconSize} />
         </a>
       </nav>
     </footer>
